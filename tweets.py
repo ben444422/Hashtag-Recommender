fca00e132143ce3cc7068b59c0ef14607cd81e9a
Rahji Abdurehman <rahji.abdu@gmail.com>
2014-04-30 05:53:47 -0400

    the program

---
+++ tweets.py
@@ -0,0 +1,62 @@
+import tweepy
+import re
+#import psycopg2
+
+api_consumer_key = 'Bl1FYcWkJ4V2TKHssLehMzfCI'
+api_consumer_secret = 'oxVLXl3aH1hrvwwA2me8yc1Ly1pdu7Gl3CHL9LK1NeU7lIFbxV'
+access_token = '359833755-jNLjWDXvSJ4TjCPNxD7bHYsCSg6DE7fBEE0vrCOZ'
+access_token_secret = '6VviM3JfGRWQh7b6jttnXvO2JGdmuBfJh4hX6JVa17Gb0'
+
+'''
+# Info supplied from twitter to develop with their API
+api_consumer_key = 'REDACTED'
+api_consumer_secret = 'REDACTED'
+access_token = 'REDACTED'
+access_token_secret = 'REDACTED'
+'''
+
+# Handle authorization with given info, connect to API
+auth = tweepy.OAuthHandler(api_consumer_key, api_consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# Next make connection with local database
+#conn = psycopg2.connect('dbname=postgretwitter user=rahjiabdurehman')
+#cur = conn.cursor()
+
+hashtags_sought = ['#surprised', '#calm', '#sad', '#happy', '#relieved',
+                   '#restless', '#thankful', '#weird', '#peaceful', '#relaxed',
+                   '#optimistic', '#loved', '#lonely', '#hyper', '#hungry',
+                   '#frustrated', '#exhausted', '#envious', '#drained', '#dark',
+                   '#crazy', '#curious', '#content', '#cheerful', '#annoyed']
+
+#sql_instruction = "INSERT INTO test (id_strs, tweet_texts) VALUES (%s, %s)"
+
+# Store the tweet's id as a string and its text in a local Postgres database
+#for hashtag in hashtags_sought:
+    #for found_tweet in tweepy.Cursor(api.search, hashtag).items(2):
+        # Ensure it's not a retweet
+with open('tweets.txt', 'w') as f:
+    for hashtag in hashtags_sought:
+        for found_tweet in tweepy.Cursor(api.search, hashtag).items(2):
+            if not re.search('RT',found_tweet.text) and not re.search('\n', found_tweet.text):
+                #cur.execute(sql_instruction, (found_tweet.id_str, found_tweet.text))
+                #try:
+                #    found_tweet.text.decode('ascii')
+                #except UnicodeDecodeError, UnicodeEncodeError:
+                #    print 'chupe'
+                #found_tweet_text_decoded = found_tweet.text.encode('utf-8')
+                #else:
+                encoded = found_tweet.text.encode('utf-8')
+                decoded = encoded.decode('utf-8')
+                print 'hi'
+                print found_tweet.id_str
+                #print found_tweet_text_decoded
+                print found_tweet.text
+                f.write(found_tweet.id_str + " x " + decoded + '\n')
+                #f.write(found_tweet.id_str + "x " + found_tweet.text + '\n')
+
+#conn.commit()
+
+#cur.close()
+#conn.close()
