8d64e8f25a3aaaf75505cd806de203a192e37234
Benjamin Chen <ben@nat-oitwireless-inside-vapornet100-a-2654.princeton.edu>
2014-05-01 18:15:35 -0400

    removed data directory. Added script to get tweets and add them to the database. added tweet library to get tweets

--- TweetGrabber.py
+++ TweetGrabber.py
@@ -1,28 +1,72 @@
 import tweepy
 import re
+import sys
+import psycopg2
+import string
 from pprint import pprint
 #import psycopg2
+
+
+def validate(tweet):
+	return tweet.lang == "en" and not re.search('RT', tweet.text) and not re.search('\n', tweet.text)
+
+# get the hashtags and the body
+def separate(tweet_text):
+	hashtags = []
+	body = ""
+	words = tweet_text.split()
+	body_count = 0
+	for word in words:
+		if word[0] == "#":
+			hashtags.append(word[1:].lower())
+		else:
+			body = body + word + " "
+			body_count = body_count + 1
+	return (hashtags, body, body_count)
 
 # remove "@" user references and urls
 def clean_tweet(tweet):
 	tokens = tweet.split()
 	new_tweet = ""
-	print tokens
 	for tok in tokens:
-		if not re.search(r'''(\.net)|(\.com)|(\.org)|(\.edu)|(\.gov)|(\.uk)|(www\.)|(http)|(@)''', tok):
+		if not re.search(r'''(\.net)|(\.com)|(\.org)|(\.edu)|(\.gov)|(\.uk)|(www\.)|(http)|(@)|(\\U)''', tok):
 			new_tweet = new_tweet + tok + " "
+	# remove words/characters that arent printable  
+	new_tweet = filter(lambda x: x in string.printable, new_tweet)
 	return new_tweet
 
 
 
 ###### PARAMETERS ###################
+
+## Twitter API Parameters
 api_consumer_key = 'Bl1FYcWkJ4V2TKHssLehMzfCI'
 api_consumer_secret = 'oxVLXl3aH1hrvwwA2me8yc1Ly1pdu7Gl3CHL9LK1NeU7lIFbxV'
 access_token = '359833755-jNLjWDXvSJ4TjCPNxD7bHYsCSg6DE7fBEE0vrCOZ'
 access_token_secret = '6VviM3JfGRWQh7b6jttnXvO2JGdmuBfJh4hX6JVa17Gb0'
 
+
+## Database Parameters
+DB_SETTINGS = {
+	'NAME': "hr",
+	'USER': "tech",
+	'PASSWORD': "keyboard",
+	'HOST': 'datainstance.c96disxtqbt5.us-east-1.rds.amazonaws.com',
+	'PORT': '5432'
+}
+
 # the number of tweets to get per hashtag in hashtags_sought
-TWEETS_PER_HASHTAG = 10
+TWEETS_PER_HASHTAG = 50
+
+# the minimum number of non hashtag words
+MIN_WORDS = 5
+
+# The hashtags we care about
+hashtags = ['surprised', 'calm', 'sad', 'happy', 'relieved',
+'restless', 'thankful', 'weird', 'peaceful', 'relaxed',
+'optimistic', 'loved', 'lonely', 'hyper', 'hungry',
+'frustrated', 'exhausted', 'envious', 'drained', 'dark',
+'crazy', 'curious', 'content', 'cheerful', 'annoyed']
 #####################################
 
 # Handle authorization with given info, connect to API
@@ -30,37 +74,80 @@ auth = tweepy.OAuthHandler(api_consumer_key, api_consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# Next make connection with local database
-#conn = psycopg2.connect('dbname=postgretwitter user=rahjiabdurehman')
-#cur = conn.cursor()
+# CONNECT to the database
+try:
+	conn = psycopg2.connect("dbname='%s' user='%s' host='%s' password='%s'" % (DB_SETTINGS['NAME'],DB_SETTINGS['USER'],DB_SETTINGS['HOST'], DB_SETTINGS['PASSWORD']))
+except:
+	print "Error: " + str(sys.exc_info()[0])
+	raise Exception("Cannot connect to database")
 
-hashtags_sought = ['#surprised', '#calm', '#sad', '#happy', '#relieved',
-'#restless', '#thankful', '#weird', '#peaceful', '#relaxed',
-'#optimistic', '#loved', '#lonely', '#hyper', '#hungry',
-'#frustrated', '#exhausted', '#envious', '#drained', '#dark',
-'#crazy', '#curious', '#content', '#cheerful', '#annoyed']
+cur = conn.cursor()
 
-#sql_instruction = "INSERT INTO test (id_strs, tweet_texts) VALUES (%s, %s)"
 
-# Store the tweet's id as a string and its text in a local Postgres database
-#for hashtag in hashtags_sought:
-    #for found_tweet in tweepy.Cursor(api.search, hashtag).items(2):
-        # Ensure it's not a retweet
+# index of the tweet grabbed. for debugging 
 tweet_index = 0
+for hashtag in hashtags:
+	for tweet in tweepy.Cursor(api.search, "#" + hashtag).items(TWEETS_PER_HASHTAG):		
+		#check if the tweet already exists. If it does don't do anything
+		tweet_id = str(tweet.id)
+		sql_tweet_check = "SELECT count(*) from tweet where tweet_id=%s"
+		cur.execute(sql_tweet_check,(tweet_id,))
+		if int(cur.fetchone()[0]) > 0:
+			continue
 
-with open('tweets.txt', 'w') as f:
-	for hashtag in hashtags_sought:
-		for found_tweet in tweepy.Cursor(api.search, hashtag).items(TWEETS_PER_HASHTAG):
-			if found_tweet.lang == "en" and not re.search('RT',found_tweet.text) and not re.search('\n', found_tweet.text):
-		        #cur.execute(sql_instruction, (found_tweet.id_str, found_tweet.text)
-		   		tweet_encoded = clean_tweet(found_tweet.text.encode('utf-8'))
-		        print "Grabbing Tweet: " + str(tweet_index) + " for hashtag: " + hashtag
-		        print "Tweet Body: " + tweet_encoded
-		        tweet_index = tweet_index + 1
-		        #f.write(found_tweet.id_str.encode('utf-8') + " x " + tweet_encoded + '\n')
-		        f.write(tweet_encoded + '\n')
-#conn.commit()
+		if validate(tweet):
+			print "Grabbing Tweet: " + str(tweet_index) + " for hashtag: " + hashtag
+			print "Tweet Body: " + tweet.text	
+			tweet_index = tweet_index + 1
+			(tweet_hashtags, tweet_body, tweet_body_count) = separate(clean_tweet(tweet.text))
+			print tweet_hashtags
+			print tweet_body
+			print "=============================="
 
-#cur.close()
-#conn.close()
+			if tweet_body_count < MIN_WORDS:
+				continue
 
+			# create an entry for the tweet in the tweet table
+			sql_tweet = "INSERT INTO tweet (tweet_id, tweet_body) VALUES (%s, %s)"
+			cur.execute(sql_tweet, (tweet_id, tweet_body))
+
+			for tweet_hashtag in tweet_hashtags:
+				sql_check = "SELECT count(*) from hashtag where hashtag_id=%s"
+				cur.execute(sql_check,(tweet_hashtag,))
+				# Check if the hashtag table has an entry for this hashtag, if not make an entry
+				if int(cur.fetchone()[0]) == 0:
+					# create an entry in the hashtag table
+					sql_hashtag = "INSERT INTO hashtag (hashtag_id) VALUES (%s)"
+					cur.execute(sql_hashtag, (tweet_hashtag,))	
+				conn.commit()
+				# Create the association in the hashtag_tweet table
+				sql_hashtag_tweet = "INSERT INTO hashtag_tweet (hashtag_id, tweet_id) VALUES (%s,%s)"
+				try:
+					cur.execute(sql_hashtag_tweet, (tweet_hashtag, tweet_id))
+				except psycopg2.IntegrityError:
+					conn.rollback()
+				else:
+					conn.commit()
+conn.close()	
+
+
+### IGNORE THIS STUFF. This was just the code to create the tables
+
+
+# CREATE TABLE hashtag(
+# hashtag_id   text PRIMARY KEY
+# );
+
+
+
+# CREATE TABLE tweet(
+#  tweet_id text PRIMARY KEY
+# ,tweet_body     text NOT NULL
+# ,tweet_date_added date NOT NULL DEFAULT now()::date
+# );
+
+# CREATE TABLE hashtag_tweet(
+#  hashtag_id    text references hashtag (hashtag_id) ON UPDATE CASCADE ON DELETE CASCADE
+# ,tweet_id text references tweet (tweet_id) ON UPDATE CASCADE
+# ,CONSTRAINT hashtag_tweet_pkey PRIMARY KEY (hashtag_id, tweet_id)  
+# );
--- TweetLib.py
+++ TweetLib.py
@@ -1,54 +1,56 @@
 # Library to get tweets from the database
 # THIS IS NOT THE FILE THAT GETS TWEETS FROM THE TWITTER API
-
 import psycopg2
 import operator
+from pprint import pprint
 
-DB_SETTINGS = {
-	NAME: "name",
-	USER: "user",
-	PASSWORD: "password",
-	HOST: "host",
-	PORT: 5432
-}
-
-
-class TweetGetter:
-	def __init__(self):
-		try:
-			self.conn = psycopg2.connect("dbname='%s' user='%s' host='%s' password='%s'" % (DB_SETTINGS['NAME'],DB_SETTINGS['USER'],DB_SETTINGS['HOST'], DB_SETTINGS['PASSWORD']))
-		except:
-			raise Exception("Cannot connect to database")
-		self.cursor = conn.cursor()
+class TweetLib:
+	## Database Parameters
+	DB_SETTINGS = {
+		'NAME': "hr",
+		'USER': "tech",
+		'PASSWORD': "keyboard",
+		'HOST': 'datainstance.c96disxtqbt5.us-east-1.rds.amazonaws.com',
+		'PORT': '5432'
+	}
 	
 	def close_db(self):
 		self.conn.close()
 	
+	def open_db(self):
+		try:
+			self.conn = psycopg2.connect("dbname='%s' user='%s' host='%s' password='%s'" % (TweetLib.DB_SETTINGS['NAME'],TweetLib.DB_SETTINGS['USER'],TweetLib.DB_SETTINGS['HOST'], TweetLib.DB_SETTINGS['PASSWORD']))
+		except:
+			raise Exception("Cannot connect to database")
+		self.cursor = self.conn.cursor()
+
 	# get the [count] hashtags with the most tweets in the database
 	# This function may be particularly expensive so don't call it too much
-	def get_top_hashtags(self, count):
-		self.cursor.execute("SELECT DISTINCT name, hashtag_id from Hashtag")
+	def get_top_hashtags(self, num_hashtags):
+		self.open_db()
+		self.cursor.execute("SELECT hashtag_id from hashtag")
 		hashtags = list(self.cursor.fetchall())
 		hashtag_counts = {}
 		for hashtag in hashtags:
-			name = hashtag[0]
-			hashtag_id = hashtag[1]
-			self.cursor.execute("SELECT COUNT(*) from Hashtag_Tweet where hashtag_id=%s", (hashtag_id,))
-			count = self.cursor.fetchall()[0][0]
-			hashtag_counts[name] = count
+			hashtag_id = hashtag[0]
+			self.cursor.execute("SELECT COUNT(*) from hashtag_tweet where hashtag_id=%s", (hashtag_id,))
+			count = int(self.cursor.fetchone()[0])
+			hashtag_counts[hashtag_id] = count
 		hashtag_counts = sorted(hashtag_counts.iteritems(), key = operator.itemgetter(1))
 		hashtag_counts.reverse()
-		return hashtag_counts[:count]
+		self.close_db()
+		return hashtag_counts[:num_hashtags]
 
 	# get [num_tweets] tweets for a specific hashtag			
 	def get_tweets(self, hashtag, num_tweets):
-		hashtag_id = self.get_hashtag_id(hashtag)
-		self.cursor.execute("SELECT Tweet.name, Tweet.date FROM Tweet INNER JOIN Hashtag_Tweet as ht ON ht.tweet_id = Tweet.tweet_id WHERE ht.hashtag_id = %s", (hashtag_id,))
-		return list(self.cursor.fetchall())
+		self.open_db()
+		hashtag_id = hashtag.lower()
+		self.cursor.execute("SELECT tweet.tweet_body FROM tweet INNER JOIN hashtag_tweet as ht ON ht.tweet_id = tweet.tweet_id WHERE ht.hashtag_id = %s", (hashtag_id,))
+		tweets = list(self.cursor.fetchall())
+		self.close_db()
+		return [tweet[0] for tweet in tweets]
 
-	# get the hashtag_id for a specific hashtag
-	def get_hashtag_id(self, hashtag):
-		self.cursor.execute("SELECT id from Hashtag WHERE name=%s LIMIT 1", (hashtag,))
-		hashtag_id = self.cursor.fetchall()[0][0]
-		return hashtag_id
 
+tl= TweetLib()
+#pprint(tl.get_top_hashtags(30))
+pprint(tl.get_tweets("peaceful", 10))
